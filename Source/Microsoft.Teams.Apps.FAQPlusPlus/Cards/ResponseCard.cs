33b2409f99b178f0f437eb4c4e79b506d1fd2289
gstewa200 <gstewa200@GACDOL-28MJ1G2>
2020-04-02 14:58:05 -0400

    Removing question line.

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/ResponseCard.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/ResponseCard.cs
@@ -38,11 +38,6 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Cards
                     },
                     new AdaptiveTextBlock
                     {
-                        Text = question,
-                        Wrap = true,
-                    },
-                    new AdaptiveTextBlock
-                    {
                         Text = answer,
                         Wrap = true,
                     },
